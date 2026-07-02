fa383fd5d76d9d5cf7569d66cb441be771065acd
adtyfrds <adityafrds82@gmail.com>
2026-07-02 19:12:30 +0700

    anak anjing aya wae

--- src/pages/onboarding/components/ProtectedRoute.jsx
+++ src/pages/onboarding/components/ProtectedRoute.jsx
@@ -2,16 +2,21 @@ import React from "react";
 import { Navigate } from "react-router-dom";
 
 const ProtectedRoute = ({ children }) => {
-  if (token && isOnboarded !== 'true') {
+  // Ambil data dari localStorage
+  const token = localStorage.getItem("token");
+  const isOnboarded = localStorage.getItem("isOnboarded");
+
+  // Belum login
   if (!token) {
     return <Navigate to="/login" replace />;
   }
 
-  if (token && !isOnboarded) {
+  // Sudah login tapi belum onboarding
+  if (isOnboarded !== "true") {
     return <Navigate to="/kuesioner" replace />;
   }
-}
 
+  // Sudah login & onboarding
   return children;
 };
 
