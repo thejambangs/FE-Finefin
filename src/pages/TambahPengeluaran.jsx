1a4a6a4da1eaa4fb462af2d5f84fac47dd009b6c
adtyfrds <adityafrds82@gmail.com>
2026-07-23 14:48:13 +0700

    mengubah header sesuai dengan dashboard

--- src/pages/TambahPengeluaran.jsx
+++ src/pages/TambahPengeluaran.jsx
@@ -176,11 +176,11 @@ const TambahPengeluaran = () => {
             </div>
 
             <div className="flex-none hidden md:block">
-              <ul className="menu menu-horizontal gap-2 text-sm font-semibold">
+              <ul className="menu menu-horizontal gap-2 text-sm">
                 <li>
                   <Link
                     to="/dashboard"
-                    className="text-indigo-600 bg-indigo-50 font-bold rounded-xl px-4 py-2"
+                    className="text-indigo-600 bg-indigo-50 rounded-xl px-4 py-2"
                   >
                     Dasbor
                   </Link>
